b6246eefc9dfe0118998a9a4120228b9fd0d7263
TristanDiabolo <tristan@diabolo-web.com>
2021-03-16 17:24:26 +0100

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import '../scss/app.scss';
-
-/* Your JS Code goes here */
-
-/* Demo JS */
-import './demo.js';
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import './components/cubeView'
