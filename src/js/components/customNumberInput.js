e28f5c45fd50ff3a1acccbe40a5f2d413b4309eb
TristanDiabolo <tristan@diabolo-web.com>
2021-03-19 12:53:38 +0100

    Custom number inputs functionnal

--- src/js/components/customNumberInput.js
+++ src/js/components/customNumberInput.js
@@ -1,2 +1,41 @@
-// const inputs = document.querySelectorAll('input[type="number"]')
-// console.log(inputs)
+const setEvents = (btn, input, elem) => {
+    const event = new Event('change');
+    input.addEventListener('change', () => {
+        elem.querySelector('.custom-number-show').innerHTML = input.value
+    })
+
+    btn.addEventListener('click', (e) => {
+        if (btn.getAttribute('data-target') === '+') {
+            input.value = parseInt(input.value) + 1
+        } else {
+            input.value = parseInt(input.value) - 1
+        }
+        input.dispatchEvent(event)
+    })
+}
+
+const setButton = (elem, input, value) => {
+    const btn = document.createElement('button')
+    btn.setAttribute('data-target', value)
+    btn.classList.add('custom-number-btn')
+    btn.appendChild(document.createTextNode(value))
+    elem.appendChild(btn)
+
+    setEvents(btn, input, elem)
+}
+
+const inputs = document.querySelectorAll('.custom-number-input')
+inputs.forEach((elem) => {
+    const input = elem.querySelector('input')
+    input.style.display = 'none'
+
+    setButton(elem, input, '-')
+
+    const showNumber = document.createElement('span')
+    showNumber.classList.add('custom-number-show')
+    showNumber.appendChild(document.createTextNode(input.value))
+    elem.appendChild(showNumber)
+
+    setButton(elem, input, '+')
+
+})
